8c479e34646ebeb28086a4e048bffae13d7af6ac
Student User <student@workstation.lab.example.com>
2022-08-07 02:58:43 -0400

    Tweak some

--- project-2/cektabel.sh
+++ project-2/cektabel.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8bab731271250a62903ea976c5c6f62198d7c90d18e8ab39f4b32b79aaf498ff
-size 98
+oid sha256:a66f2d2c879319b074eb7ad2e5aa80b6ee6706e1fbf98facb424399c8ecc520a
+size 99
--- project-2/snrpod.sh
+++ project-2/snrpod.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:681659a59ff04b1660d725bbaee5c65ccdedc70457bb0b9c71e4b76cb9d1ce34
-size 61
+oid sha256:f07a1b1cbb02819676ccb0709e3054b75335039f3868f3f3e0464148cb4d74f4
+size 64
